c1cc42400815d2aa092ff2d8d80454793af1b9b8
ggzor <30713864+ggzor@users.noreply.github.com>
2020-10-25 22:47:32 -0600

    Agregando librería Golden T

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,4 +1,19 @@
-public class App {
+import com.golden.gamedev.Game;
+import java.awt.*;
+
+public class App extends Game {
+  public void initResources() {
+    // initialize game variables
+  }
+
+  public void update(long elapsedTime) {
+    // update the game variables
+  }
+
+  public void render(Graphics2D g) {
+    // render the game to the screen
+  }
+
   public static void main(String[] args) throws Exception {
     System.out.println("Hello, World!");
   }
