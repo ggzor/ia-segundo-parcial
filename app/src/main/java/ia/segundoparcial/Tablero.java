00fdd282eecfd9253ceec98a54bb270921207911
isai-samir <60807405+isai-samir@users.noreply.github.com>
2020-10-30 22:33:38 -0600

    Agregando implementación de consecutivos

--- app/src/main/java/ia/segundoparcial/Tablero.java
+++ app/src/main/java/ia/segundoparcial/Tablero.java
@@ -54,7 +54,286 @@ public class Tablero {
   }
 
   public int obtenerConsecutivos(int n, Celda jugador) {
-    return 0;
+    int contador, fila, columna, k;
+    boolean bandera;
+    contador = columna = 0;
+    // Buscar Horizontal
+    fila = FILAS - 1;
+    while (fila >= 0) {
+      columna = 0;
+      while (columna < COLUMNAS) {
+        if (celdas[fila][columna] == jugador) {
+          bandera = false;
+          k = 0;
+          if (columna != 0) if (celdas[fila][columna - 1] == Celda.Vacio) bandera = !bandera;
+          while (celdas[fila][columna] == jugador) {
+            k++;
+            columna++;
+            if (columna == COLUMNAS) break;
+          }
+          if (k == n && bandera) contador++;
+          else if (columna != COLUMNAS && k == n)
+            if (celdas[fila][columna] == Celda.Vacio) contador++;
+        }
+        columna++;
+      }
+      fila--;
+    }
+    // Buscar en vertical
+    columna = 0;
+    while (columna < COLUMNAS) {
+      fila = FILAS - 1;
+      while (fila >= 0) {
+        if (celdas[fila][columna] == jugador) {
+          k = 0;
+          while (celdas[fila][columna] == jugador) {
+            k++;
+            fila--;
+            if (fila == -1) break;
+          }
+          if (fila != -1 && k == n) if (celdas[fila][columna] == Celda.Vacio) contador++;
+        }
+        fila--;
+      }
+      columna++;
+    }
+    // Buscar en diagonal (\)
+    // Buscar en la primera mitad (\)
+    int auxCelda;
+    columna = 0;
+    while (columna < COLUMNAS) {
+      fila = FILAS - 1;
+      auxCelda = columna;
+      while (auxCelda >= 0 && fila >= 0) {
+        if (celdas[fila][auxCelda] == jugador) {
+          bandera = false;
+          k = 0;
+          if (fila != FILAS - 1)
+            if (celdas[fila + 1][auxCelda + 1] == Celda.Vacio) bandera = !bandera;
+          while (celdas[fila][auxCelda] == jugador) {
+            fila--;
+            auxCelda--;
+            k++;
+            if (auxCelda == -1 || fila == -1) break;
+          }
+          if (k == n && bandera) contador++;
+          else if (auxCelda != -1 && fila != -1 && k == n)
+            if (celdas[fila][auxCelda] == Celda.Vacio) contador++;
+        }
+        fila--;
+        auxCelda--;
+      }
+      columna++;
+    }
+    // Busca la 2 mitad (\)
+    fila = FILAS - 2;
+    while (fila >= 0) {
+      auxCelda = fila;
+      columna = COLUMNAS - 1;
+      while (auxCelda >= 0 && columna >= 0) {
+        if (celdas[fila][auxCelda] == jugador) {
+          bandera = false;
+          k = 0;
+          if (columna != COLUMNAS - 1)
+            if (celdas[auxCelda + 1][columna + 1] == Celda.Vacio) bandera = !bandera;
+          while (celdas[auxCelda][columna] == jugador) {
+            columna--;
+            auxCelda--;
+            k++;
+            if (auxCelda == -1 || columna == -1) break;
+          }
+          if (k == n && bandera) contador++;
+          else if (auxCelda != -1 && columna != -1 && k == n)
+            if (celdas[auxCelda][columna] == Celda.Vacio) contador++;
+        }
+        columna--;
+        auxCelda--;
+      }
+      fila--;
+    }
+    // Buscar en diagonal (/)
+    // Busca en la 1 mitad (/)
+    columna = COLUMNAS - 1;
+    while (columna >= 0) {
+      fila = FILAS - 1;
+      auxCelda = columna;
+      while (auxCelda < COLUMNAS && fila >= 0) {
+        if (celdas[fila][auxCelda] == jugador) {
+          bandera = false;
+          k = 0;
+          if (fila != FILAS - 1)
+            if (celdas[fila + 1][auxCelda - 1] == Celda.Vacio) bandera = !bandera;
+          while (celdas[fila][auxCelda] == jugador) {
+            fila--;
+            auxCelda++;
+            k++;
+            if (auxCelda == COLUMNAS || fila == -1) break;
+          }
+          if (k == n && bandera) contador++;
+          else if (auxCelda != COLUMNAS && fila != -1 && k == n)
+            if (celdas[fila][auxCelda] == Celda.Vacio) contador++;
+        }
+        fila--;
+        auxCelda++;
+      }
+      columna--;
+    }
+    // Busca en la 2 mitad (/)
+    fila = FILAS - 2;
+    while (fila >= 0) {
+      auxCelda = fila;
+      columna = 0;
+      while (auxCelda >= 0 && columna < COLUMNAS) {
+        if (celdas[auxCelda][columna] == jugador) {
+          bandera = false;
+          k = 0;
+          if (columna != 0)
+            if (celdas[auxCelda + 1][columna - 1] == Celda.Vacio) bandera = !bandera;
+          while (celdas[auxCelda][columna] == jugador) {
+            columna++;
+            auxCelda--;
+            k++;
+            if (auxCelda == -1 || columna == COLUMNAS) break;
+          }
+          if (k == n && bandera) contador++;
+          else if (auxCelda != -1 && columna != COLUMNAS && k == n)
+            if (celdas[auxCelda][columna] == Celda.Vacio) contador++;
+        }
+        columna++;
+        auxCelda--;
+      }
+      fila--;
+    }
+    return contador;
+  }
+
+  public boolean esGanador(Celda jugador) {
+    int fila, columna, k, ganadorNum;
+    ganadorNum = 4;
+    // Busca en horizontal
+    fila = FILAS - 1;
+    while (fila >= 0) {
+      columna = 0;
+      while (columna < COLUMNAS) {
+        if (celdas[fila][columna] == jugador) {
+          k = 0;
+          while (celdas[fila][columna] == jugador) {
+            k++;
+            columna++;
+            if (columna == COLUMNAS) break;
+          }
+          if (k == ganadorNum) return true;
+        }
+        columna++;
+      }
+      fila--;
+    }
+    // Buscar en vertical
+    columna = 0;
+    while (columna < COLUMNAS) {
+      fila = FILAS - 1;
+      while (fila >= 0) {
+        if (celdas[fila][columna] == jugador) {
+          k = 0;
+          while (celdas[fila][columna] == jugador) {
+            k++;
+            fila--;
+            if (fila == -1) break;
+          }
+          if (k == ganadorNum) return true;
+        }
+        fila--;
+      }
+      columna++;
+    }
+    // Buscar en diagonal (\)
+    // Buscar en la primera mitad (\)
+    int auxCelda;
+    columna = 0;
+    while (columna < COLUMNAS) {
+      fila = FILAS - 1;
+      auxCelda = columna;
+      while (auxCelda >= 0 && fila >= 0) {
+        if (celdas[fila][auxCelda] == jugador) {
+          k = 0;
+          while (celdas[fila][auxCelda] == jugador) {
+            fila--;
+            auxCelda--;
+            k++;
+            if (auxCelda == -1 || fila == -1) break;
+          }
+          if (k == ganadorNum) return true;
+        }
+        fila--;
+        auxCelda--;
+      }
+      columna++;
+    }
+    // Busca la 2 mitad (\)
+    fila = FILAS - 2;
+    while (fila >= 0) {
+      auxCelda = fila;
+      columna = COLUMNAS - 1;
+      while (auxCelda >= 0 && columna >= 0) {
+        if (celdas[fila][auxCelda] == jugador) {
+          k = 0;
+          while (celdas[auxCelda][columna] == jugador) {
+            columna--;
+            auxCelda--;
+            k++;
+            if (auxCelda == -1 || columna == -1) break;
+          }
+          if (k == ganadorNum) return true;
+        }
+        columna--;
+        auxCelda--;
+      }
+      fila--;
+    }
+    // Buscar en diagonal (/)
+    // Busca en la 1 mitad (/)
+    columna = COLUMNAS - 1;
+    while (columna >= 0) {
+      fila = FILAS - 1;
+      auxCelda = columna;
+      while (auxCelda < COLUMNAS && fila >= 0) {
+        if (celdas[fila][auxCelda] == jugador) {
+          k = 0;
+          while (celdas[fila][auxCelda] == jugador) {
+            fila--;
+            auxCelda++;
+            k++;
+            if (auxCelda == COLUMNAS || fila == -1) break;
+          }
+          if (k == ganadorNum) return true;
+        }
+        fila--;
+        auxCelda++;
+      }
+      columna--;
+    }
+    // Busca en la 2 mitad (/)
+    fila = FILAS - 2;
+    while (fila >= 0) {
+      auxCelda = fila;
+      columna = 0;
+      while (auxCelda >= 0 && columna < COLUMNAS) {
+        if (celdas[auxCelda][columna] == jugador) {
+          k = 0;
+          while (celdas[auxCelda][columna] == jugador) {
+            columna++;
+            auxCelda--;
+            k++;
+            if (auxCelda == -1 || columna == COLUMNAS) break;
+          }
+          if (k == ganadorNum) return true;
+        }
+        columna++;
+        auxCelda--;
+      }
+      fila--;
+    }
+    return false;
   }
 
   public static Optional<Tablero> deArreglo(String[] a) {
@@ -65,7 +344,33 @@ public class Tablero {
     if (!Arrays.stream(a)
         .allMatch(fila -> fila.chars().allMatch(c -> c == '1' || c == '2' || c == ' ')))
       return Optional.empty();
-
-    return Optional.of(new Tablero());
+    for (int j = COLUMNAS - 1; j >= 0; j--)
+      for (int i = 0; i < FILAS; i++) {
+        if (a[i].charAt(j) != ' ') {
+          int k = i;
+          while (k < FILAS) {
+            if (a[k].charAt(j) == ' ') return Optional.empty();
+            k++;
+          }
+          break;
+        }
+      }
+    // exitOptional.of(new Tablero());
+    Tablero t = new Tablero();
+    for (int i = 0; i < FILAS; i++)
+      for (int j = 0; j < COLUMNAS; j++) {
+        switch (a[i].charAt(j)) {
+          case ' ':
+            t.celdas[i][j] = Celda.Vacio;
+            break;
+          case '1':
+            t.celdas[i][j] = Celda.Jugador1;
+            break;
+          case '2':
+            t.celdas[i][j] = Celda.Jugador2;
+            break;
+        }
+      }
+    return Optional.of(t);
   }
 }
